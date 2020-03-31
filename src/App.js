43ca69d2c2c3bc0c91fc6e81414fb625dac0cc46
Tairema Dailey <tai@tairemadailey.com>
2020-03-30 23:40:54 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+    <section className="name">
+      <h1>Tairema Dailey</h1>
+      <h3>Front-End Developer</h3>
+    </section>
+  </main>
   );
 }
 
