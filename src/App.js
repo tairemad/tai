9008c0c218c1cf7fdfe4510f90f904e0b0ad8f7a
Tairema Dailey <tai@tairemadailey.com>
2020-04-02 19:38:50 -0400

    updating styles

--- src/App.js
+++ src/App.js
@@ -7,25 +7,22 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <section className="top">
-        <section className="status">
-          <p><span className="pulse"></span> Available for Freelance Work Only</p>
-        </section>
-        <section className="icons">
-          <a href="https://twitter.com/tairemadailey" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faTwitter} size="lg" className="svg"/></a>
-          <a href="https://www.facebook.com/tairemadailey" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faFacebookF} size="lg" className="svg" /></a>
-          <a href="https://www.linkedin.com/in/tairema" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faLinkedinIn} size="lg" className="svg" /></a>
-          <a href="https://github.com/tairemad" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faGithub} size="lg" className="svg" /></a>
-          <a href="mailto:tai@tairemadailey.com" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faAt} size="lg" className="svg" /></a>
-        </section>
-
+      <section className="icons">
+        <a href="https://twitter.com/tairemadailey" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faTwitter} size="lg" className="svg"/></a>
+        <a href="https://www.facebook.com/tairemadailey" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faFacebookF} size="lg" className="svg" /></a>
+        <a href="https://www.linkedin.com/in/tairema" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faLinkedinIn} size="lg" className="svg" /></a>
+        <a href="https://github.com/tairemad" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faGithub} size="lg" className="svg" /></a>
+        <a href="mailto:tai@tairemadailey.com" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faAt} size="lg" className="svg" /></a>
       </section>
       <section className="container">
         <section className="name">
           <h1>Tairema Dailey</h1>
           <h3>Front-End Developer</h3>
         </section>
-    </section>
+        <section className="status">
+          <p><span className="pulse"></span> Available for Freelance Work Only</p>
+        </section>
+      </section>
   </React.Fragment>
   );
 }
