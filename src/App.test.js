8e6e5ff26860ae03fcc1c3f295a290921f13fdf1
tairemad <tai@tairemadailey.com>
2022-09-10 13:24:24 -0400

    updating test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Tairema Dailey', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/Tairema Dailey/i);
+  expect(header).toBeInTheDocument();
 });
