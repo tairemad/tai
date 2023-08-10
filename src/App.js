dcd3ae7968e108d74becc00b7ddd18c70d8170f6
Tairema Dailey <tai@tairemadailey.com>
2023-08-09 21:29:55 -0400

    removing fb adding res

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { faAt } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faAt, faFile } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
   return (
     <>
       <section className="icons">
-        <a href="https://www.facebook.com/tairemadailey" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faFacebookF} size="lg" className="svg" /></a>
-        <a href="https://www.linkedin.com/in/tairema" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faLinkedinIn} size="lg" className="svg" /></a>
-        <a href="https://github.com/tairemad" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faGithub} size="lg" className="svg" /></a>
-        <a href="mailto:tai@tairemadailey.com" target="_blank" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faAt} size="lg" className="svg" /></a>
+        <a href="https://drive.google.com/file/d/15ZZQ_VZlnwnZQEvidA31cCIzNl2z9gWF/view?usp=sharing" target="_blank" tabindex="1" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faFile} size="lg" className="svg" /></a>
+        <a href="https://www.linkedin.com/in/tairema" target="_blank" tabindex="2" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faLinkedinIn} size="lg" className="svg" /></a>
+        <a href="https://github.com/tairemad" target="_blank" tabindex="3" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faGithub} size="lg" className="svg" /></a>
+        <a href="mailto:tai@tairemadailey.com" target="_blank" tabindex="4" rel="noopener noreferrer" className="links"><FontAwesomeIcon icon={faAt} size="lg" className="svg" /></a>
       </section>
       <section className="container">
         <section className="name">
